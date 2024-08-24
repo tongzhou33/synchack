a9aac2831bfb2bbd897e3961dd36576e450df252
jaden <monsterhulk2@gmail.com>
2024-08-24 14:38:07 +1000

    [Setu]p postpage]
    .
    /

---
+++ frontend/src/component/postpage/postpage.js
@@ -0,0 +1,14 @@
+import styles from './style/postpage.module.css';
+
+function postpage() {
+
+  return (
+    <section>
+        <h1 className={styles.title}> Welcome to the league of draven</h1>
+        <div> HI </div> 
+    </section>
+    
+  );
+}
+
+export default postpage;
